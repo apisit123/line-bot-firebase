4ab1885706dbd718d9095fd7ac7700d25cb121e5
apisit123 <aew.apisit@gmail.com>
2018-01-12 15:59:26 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -41,6 +41,9 @@ use LINE\LINEBot\MessageBuilder\TemplateBuilder\ImageCarouselColumnTemplateBuild
 
 $strAccessToken = "+eU+zQe8QJL9BraZ55TJLLTtUNQ1jDojYN63o5t3Skx2cnTqmXrr5lJNXUNBGVM8mSCtidORd7MgL6neDJf5uI5gKWhR3eUiKuqGNCdh/1ptR4Fdig9RCNHJo9tZUNJjjhH3N+MAtzE3+YVeAjlRIgdB04t89/1O/w1cDnyilFU=";
 
+$httpClient = new CurlHTTPClient(LINE_MESSAGE_ACCESS_TOKEN);
+$bot = new LINEBot($httpClient, array('channelSecret' => LINE_MESSAGE_CHANNEL_SECRET));
+
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 
@@ -62,6 +65,19 @@ $url = 'https://api.mlab.com/api/1/databases/tstdb/collections/linebot?apiKey='.
 $json = file_get_contents('https://api.mlab.com/api/1/databases/tstdb/collections/linebot?apiKey='.$api_key.'&q={"No":"'.$_msg.'"}');
 
 
+$response = $bot->getProfile($userId);
+
+if ($response->isSucceeded()) {
+  $userData = $response->getJSONDecodedBody(); // return array    
+  $dispName = $userData['displayName'];
+  } else{
+      $dispName = "Null";
+  }
+
+
+
+/*
+
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/v2/bot/profile/'.$_uid.'');
@@ -69,7 +85,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
 $headers = array();
-$headers[] = "+eU+zQe8QJL9BraZ55TJLLTtUNQ1jDojYN63o5t3Skx2cnTqmXrr5lJNXUNBGVM8mSCtidORd7MgL6neDJf5uI5gKWhR3eUiKuqGNCdh/1ptR4Fdig9RCNHJo9tZUNJjjhH3N+MAtzE3+YVeAjlRIgdB04t89/1O/w1cDnyilFU=";
+$headers[] = "Authorization: Bearer {channel access token}";
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
@@ -79,6 +95,10 @@ if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
 curl_close ($ch);
+
+*/
+
+
 
 if (strpos($_msg, 'Order') !== false) {
   if (strpos($_msg, 'Order') !== false) {
