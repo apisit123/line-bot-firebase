03950ff3703982d88afe13d9ccaad58c425381a0
apisit123 <aew.apisit@gmail.com>
2018-01-12 16:15:10 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -112,7 +112,7 @@ if (strpos($_msg, 'Order') !== false) {
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนครับ'.$result.'';
+    $arrPostData['messages'][0]['text'] = 'ขอบคุณที่สอนครับ'.$result['displayName'].'';
   }
 }else{
   if($isData >0){
