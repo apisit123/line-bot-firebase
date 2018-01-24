c2ee2505af43fb65107e37948a273d53c2c310aa
apisit123 <aew.apisit@gmail.com>
2018-01-24 23:17:34 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -52,7 +52,7 @@ $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 $_msg = $arrJson['events'][0]['message']['text'];
 $_uid = $arrJson['events'][0]['source']['userId'];
-$_rId = $arrJson['events'][0]['message'];
+$_rId = $arrJson['events'][0]['source'];
 
 $data = json_decode($json);
 $isData=sizeof($data);
